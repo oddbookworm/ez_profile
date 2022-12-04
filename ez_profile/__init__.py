536b479131090b6ef68e8c1ba3069ffed578113a
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-12-04 14:57:27 -0600

    slight cleanup

--- ez_profile/__init__.py
+++ ez_profile/__init__.py
@@ -2,15 +2,12 @@ from sys import argv
 if "--ignore" not in argv:
     from os import popen
     import sys
-    #from . import snakeviz_launcher
 
-    print(f'Running command: "{sys.executable}" -m cProfile -o stats.prof "{argv[0]}" --ignore')
     proc = popen(f'"{sys.executable}" -m cProfile -o stats.prof "{argv[0]}" --ignore')
     print(proc.read())
     proc.close()
 
     print("launching snakeviz. Ctrl-C to close")
-    print(f'Running command: "{sys.executable}" -m snakeviz stats.prof')
     proc = popen(f'"{sys.executable}" -m snakeviz stats.prof')
     proc.read()
     proc.close()
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages, setup
 
+__ver = "0.1.3"
+
 setup(
     name="ez_profile",
     packages=find_packages(include=["ez_profile"]),
-    version="0.1.2",
+    version=__ver,
     description="A basic wrapper around cProfile with optional snakeviz integration",
     author="oddbookworm",
     author_email="andrewryancoffey@hotmail.com",
     license="MIT",
     install_requires=['snakeviz'],
     url="https://github.com/oddbookworm/ez_profile",
-    download_url="https://github.com/oddbookworm/ez_profile/archive/refs/tags/v0.1.1.tar.gz",
+    download_url="https://github.com/oddbookworm/ez_profile/archive/refs/tags/v{__ver}.tar.gz",
     keywords=["ez_profile"],
     classifiers=[],
 )
