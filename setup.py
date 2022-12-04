60644c1fc8225d391429c022bcd805ec31f5629c
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-12-03 23:10:47 -0600

    pypi tweaks

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='ez_profile',
     packages=find_packages(include=['ez_profile']),
-    version='0.1.0',
+    version='0.1.1',
     description='A basic wrapper around cProfile with optional snakeviz integration',
     author='oddbookworm',
+    author_email='andrewryancoffey@hotmail.com',
     license='MIT',
-    install_requires=[]
+    install_requires=[],
+    url='https://github.com/oddbookworm/ez_profile',
+    download_url='https://github.com/oddbookworm/ez_profile/archive/refs/tags/v0.1.1.tar.gz',
+    keywords=['ez_profile'],
+    classifiers=[]
 )
